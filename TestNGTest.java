ddf45f27799a7c0b9a8d153fb5cad7a3324723b3
nurnahidhasan <nurnahidhasan@gmail.com>
2019-05-17 16:32:22 +0600

    testNGTest new work

---
+++ TestNGTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class TestNGTest {
+
+	
+		
+	    @Test
+
+	      public void secondTest(){
+	      	WebDriver w= new FirefoxDriver();
+	      	w.get("www.google.com");
+	      	
+	      
+
+
+	}
+
+}
